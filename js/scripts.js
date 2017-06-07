6f9645815e188e25b8919bb24c9b8458f71306b5
Alena Golovina and Esvethlynna Pangelinan <esvethlynna@>
2017-06-07 16:46:19 -0700

    fixed first part of the condition

--- js/scripts.js
+++ js/scripts.js
@@ -11,18 +11,26 @@ $(document).ready(function() {
    var vowels = ['a', 'e', 'i', 'o', 'u'];
 
    if (len === 1) {
-    for (var i = 0; i < vowels.length; i++) {   // loop through vowels
-      for (var j = 0; j < 1; j++) {
-        if (vowels[i] === sentenceArray[j]) {
-          sentencePigLatin = sentenceArray + "way";
-          return alert (sentencePigLatin);
-        } else {
-          sentencePigLatin = sentenceArray + "ay";
-          return alert (sentencePigLatin);
-        }
-
+     if ((sentenceArray[0] === "a") || (sentenceArray[0]=== "e") || (sentenceArray[0] === "i") || (sentenceArray[0] === "o") || (sentenceArray[0] === "u")) {
+        sentencePigLatin = sentenceInput + "way";
+        return alert (sentencePigLatin);
+      } else {
+        sentencePigLatin = sentenceInput + "ay";
+        return alert(sentencePigLatin);
       }
-    }
+    //for (var i = 0; i < vowels.length; i++) {   // loop through vowels
+    //  for (var j = 0; j < 1; j++) {
+    //    if (vowels[i] === sentenceArray[j]) {
+    //      sentencePigLatin = sentenceArray + "way";
+    //      return alert (sentencePigLatin);
+    //    } else {
+    //      sentencePigLatin = sentenceArray + "ay";
+    //      return alert (sentencePigLatin);
+    //    }
+    //  }
+    //}
+  //} else {
+
   }
 
   if (len > 1) {
@@ -44,10 +52,7 @@ $(document).ready(function() {
           //console.log(letterArray[j]);
           //wordInput = wordInput.replace(letterArray[j], '-');
 
-   //if ((sentenceArray[0] === "a") || (sentenceArray[0]=== "e") || (sentenceArray[0] === "i") || (sentenceArray[0] === "o") || (sentenceArray[0] === "u")) {
-      // sentencePigLatin = sentenceInput + "way";
-    // } else {
-    //   sentencePigLatin = sentenceInput + "ay";
+   //
 
     // }
 
