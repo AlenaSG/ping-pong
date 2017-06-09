87569641629ff6137966e8f7a6f174c0e4478016
Alena Golovina <alenasgolovina@gmail.com>
2017-06-09 10:32:10 -0700

    add js code to turn inputted strings to numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,88 +1,21 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
 
-  var numberInput = $("input#inputNum").val();
+  var stringInput = $("input#inputNum").val();
+  var numberInput = parseInt(stringInput);
 
-  //var sentenceArray = sentenceInput.split("");
+  //var mySumm = numberInput + 1
+
+
+  //alert (numberInput + 1);
+//var numberInput = var stringInput;
+  //
    //var sentencePigLatin = "";
 
+//for (var index = 0; index < numberArray.length; index ++) {
 $(".result").text(numberInput);
+//}
 event.preventDefault();
 
  });
 });
-
-   //var len = sentenceInput.length
-
-  //  alert (sentenceArray);
-
-   //var vowels = ['a', 'e', 'i', 'o', 'u'];
-
-   //if (len === 1) {
-     //if ((sentenceArray[0] === "a") || (sentenceArray[0]=== "e") || (sentenceArray[0] === "i") || (sentenceArray[0] === "o") || (sentenceArray[0] === "u")) {
-      //  sentencePigLatin = sentenceInput + "way";
-      //  return alert (sentencePigLatin);
-    //  } else {
-      //  sentencePigLatin = sentenceInput + "ay";
-      //  return alert(sentencePigLatin);
-      //}
-    //for (var i = 0; i < vowels.length; i++) {   // loop through vowels
-    //  for (var j = 0; j < 1; j++) {
-    //    if (vowels[i] === sentenceArray[j]) {
-    //      sentencePigLatin = sentenceArray + "way";
-    //      return alert (sentencePigLatin);
-    //    } else {
-    //      sentencePigLatin = sentenceArray + "ay";
-    //      return alert (sentencePigLatin);
-    //    }
-    //  }
-    //}
-  //} else {
-
-  //}
-
-  //if (len > 1) {
-   //for (var i = 0; i < vowels.length; i++) {   // loop through vowels
-    // for (var j = 0; j < 1; j++) {
-      // if (vowels[i] === sentenceArray[j]) {
-        // sentencePigLatin = sentenceInput+ "way";
-        // return alert (sentencePigLatin);
-      // } else {
-         //move the consonant to the end, then add "ay"
-        // var consonant = sentenceArray[j];
-        // var newSentence = sentenceArray.slice(1, sentenceArray.length).join('') + consonant + "ay";
-        // return alert (newSentence);
-      // }
-      //}
-    //}
-  //}
-
-          //console.log(letterArray[j]);
-          //wordInput = wordInput.replace(letterArray[j], '-');
-
-   //
-
-    // }
-
-
-
-
-
-
-
-    //var str = $("input#sentence").val();
-    //var len = sentenceInput.length;
-    //for(i=0;i<len;i++) {
-    //  var letter = sentenceInput.charAt(i);
-    //  sentenceArray.push(letter);
-    //}
-
-    //var sentenceLength = sentenceArray.length;
-    //var newSentence = "";
-  //  for(index = 0; index < sentenceLength; index +=1) {
-    //  if ((sentenceArray[index] === "a") || (sentenceArray[index] === "e") || (sentenceArray[index] === "i") || (sentenceArray[index] === "o") || (sentenceArray[index] === "u")) {
-      //  sentenceArray[index] = "-";
-    //  }
-    //  newSentence += sentenceArray[index];
-  //  }
-    //$(".newSentence").text(newSentence);
